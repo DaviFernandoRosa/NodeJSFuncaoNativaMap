76af61c0474aae0fde52386a8d8facbcbe67e337
Davi Fernando Rosa <davir0514@gmail.com>
2021-06-06 12:00:23 -0300

    Arquivo

---
+++ arraymap.js
@@ -0,0 +1,9 @@
+
+
+
+
+// funcão Nativa para mapear o array 
+
+const diasDaSemana = ["domingo", "segunda-feira", "terça-feira", "Quarta-feira", "Quinta-feira"];
+diasDaSemana.map( (dia)=> console.log(dia));
+
